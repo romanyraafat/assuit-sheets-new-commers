fff448fae4db1103219622b2fd8baf56514ecc18
Romany Raafat <romanyraafat2004@gmail.com>
2026-01-07 09:54:29 +0200

    B. Basic Data Types

---
+++ Data_Types_Condition/B_Basic_Data_Types.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int integar;
+    float decimal;
+    char character;
+    double largeDecimal;
+    long long longIntegar;
+    cin >> integar >> longIntegar >> character >> decimal >> largeDecimal;
+    cout << integar << "\n"
+         << longIntegar << "\n"
+         << character << "\n"
+         << decimal << "\n"
+         << largeDecimal << endl;
+    return 0;
+}
